24674d251d8fa297129e22288887c5bf397365db
Mahesh Raveendranatha Panicker <52023898+maheshpanickeriitpkd@users.noreply.github.com>
2024-01-22 16:43:09 +0800

    Update image_live_facial_landmarks.py

--- Codes/image_live_facial_landmarks.py
+++ Codes/image_live_facial_landmarks.py
@@ -1,5 +1,5 @@
 import cv2
-import face_recognition
+import face_recognition # Refer to https://github.com/ageitgey/face_recognition
 
 
 
@@ -19,6 +19,7 @@ while True:
         # resizing for faster detection
         frame = cv2.resize(frame, (256, 256)) #Uncomment and see the speed up
         
+        #Extract face locations using the face_recogniton library
         face_locations = face_recognition.face_locations(frame)
               
         #Draw a rectangle around the face
@@ -26,6 +27,7 @@ while True:
             top, right, bottom, left = face_location
             cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
         
+        # Extract the landmarks based on the standard face model
         landmarks = face_recognition.face_landmarks(frame,[face_location])[0]
         
         #Draw circles for each facial landmark
