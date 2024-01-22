eb4a0181e95bb210985679239d35f5b76b5a7c7b
Mahesh Raveendranatha Panicker <52023898+maheshpanickeriitpkd@users.noreply.github.com>
2024-01-22 15:51:36 +0800

    Update image_hog_feature.py

--- Codes/image_hog_feature.py
+++ Codes/image_hog_feature.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-from skimage import feature
+from skimage import feature # Refer to https://scikit-image.org/ for full capabalities of scikit-image library
 from skimage import exposure
 
 
@@ -18,22 +18,25 @@ while True:
         ret, frame = cap.read()  
         
         # resizing for faster detection
-        #frame = cv2.resize(frame, (140, 140)) #Uncomment and see the speed up
+        #frame = cv2.resize(frame, (256, 256)) #Uncomment and see the speed up
         
+        # Converting to gray scal as HOG feature extraction in scikit-image works only on gray scale image
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         
+        # Extact the HoG featues from the image
         (H, hogImage) = feature.hog(image, orientations=9, pixels_per_cell=(8, 8),
     	cells_per_block=(2, 2), transform_sqrt=True, block_norm="L1",
     	visualize=True)
-        hogImage = exposure.rescale_intensity(hogImage, out_range=(0, 255))
+        
+        # Rescale intensity to be within 0-255 (contrast stretching)
+        hogImage = exposure.rescale_intensity(hogImage, out_range=(0, 255))        
         hogImage = hogImage.astype("uint8")
-        hogImg = np.uint8(np.zeros((np.shape(hogImage)[0],np.shape(hogImage)[1],3)))
-        hogImg[:,:,0] = hogImage
-        hogImg[:,:,1] = hogImage
-        hogImg[:,:,2] = hogImage
         
-        catImg = cv2.hconcat([frame,hogImg])
+        # Converting gray to RGB
+        hogImg = cv2.cvtColor(hogImage,cv2.COLOR_GRAY2RGB)
         
+        # Horizontal concatenation to show both input image and its HoG features.
+        catImg = cv2.hconcat([frame,hogImg])        
         cv2.imshow("HOG Image", catImg)
         
         if cv2.waitKey(10) == ord('q'):
