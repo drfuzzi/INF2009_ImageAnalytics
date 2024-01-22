71e6ef85550fbae583390b884624f3e5f0c60d89
Mahesh Raveendranatha Panicker <52023898+maheshpanickeriitpkd@users.noreply.github.com>
2024-01-22 16:30:51 +0800

    Update image_face_capture.py

--- Codes/image_face_capture.py
+++ Codes/image_face_capture.py
@@ -1,10 +1,9 @@
 import cv2
 import dlib
-import matplotlib.pyplot as plt
 
 
-# dlib’s HOG + Linear SVM face detector
-detector = dlib.get_frontal_face_detector()
+# dlib’s HOG + Linear SVM face detector (There is a CNN detector which is compute intensive)
+detector = dlib.get_frontal_face_detector() # Refer to http://dlib.net/face_detector.py.html for more information
 
 
 #%% Open CV Video Capture and frame analysis
@@ -26,14 +25,17 @@ while True:
         # Convert the image to grayscale (Dlib works with grayscale images)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         
+        # Use the Dlib HoG + SVM for face detection
         faces = detector(gray)
         
+        # Extract face boundary and plot a rectangle bounding box
         for face in faces:
             x, y, w, h = (face.left(), face.top(), face.width(), face.height())
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         
-        
+        # Show the image
         cv2.imshow("frame",cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))    
+        
         
         if cv2.waitKey(10) == ord('q'):
            break
