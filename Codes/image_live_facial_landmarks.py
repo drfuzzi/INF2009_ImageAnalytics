88371a6485b8fc74eba952cb4afb4baedade8c67
Mahesh Raveendranatha Panicker <52023898+maheshpanickeriitpkd@users.noreply.github.com>
2024-01-29 13:01:20 +0800

    Update image_live_facial_landmarks.py

--- Codes/image_live_facial_landmarks.py
+++ Codes/image_live_facial_landmarks.py
@@ -38,7 +38,7 @@ while True:
         
         cv2.imshow("frame",frame)    
         
-        if cv2.waitKey(10) == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
            break
         
     
